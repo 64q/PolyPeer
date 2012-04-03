4c25604978bdcacbc0952d9006f4516765e56170
KenTiN <quentin.lebourgeois@gmail.com>
2012-04-03 23:49:48 +0200

    modification affichage console

--- webserver/src/server.cpp
+++ webserver/src/server.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-	// Récupération du serveur web
-	WebServer* server = WebServer::getInstance();
-	
 	cout << "+--------------------+" << endl;
 	cout << "| WebServer PolyPeer |" << endl;
 	cout << "+--------------------+" << endl;
+	
+	// Récupération du serveur web
+	WebServer* server = WebServer::getInstance();
 	
 	// Lancement du serveur
 	cout << "(server) Lancement du serveur sur le port 6969 ..." << endl;
