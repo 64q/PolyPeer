4b41b8e0ae5577890b207d5eccae689fc71fa9a6
KenTiN <quentin.lebourgeois@gmail.com>
2012-04-05 21:22:19 +0200

    maj generale 3, ajout Makefile, correction emplacement main()

---
+++ tcp/src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+#include "../include/ServerSocket.hpp"
+
+using namespace std;
+
+int main()
+{
+
+	ServerSocket server(5556);
+	cout << "en attente" << endl;
+	
+	Socket sock;
+	while(true)
+	{
+		sock = server.accept();
+		sock.send("crotte");
+		sock.close();
+	}
+	
+	server.close();
+
+	//    Socket sock("localhost", 5555);
+	//
+	//    char buff[200];
+	//    int taille = sock.read(buff, 200);
+	//    cout << taille<<" : "<<buff << endl;
+	//    sock.close();
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
