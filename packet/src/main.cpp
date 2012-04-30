163be0dae27541ff74377af3b97c81eab7d20418
olivier <olivier.oblin@gmail.com>
2012-04-30 13:39:55 +0200

    pret pour test des chunks

--- packet/src/main.cpp
+++ packet/src/main.cpp
@@ -34,33 +34,34 @@ int callbackSendOperation (Packet& p)
 
 int main ()
 {
-	// Création du manager
+
+// Création du manager
 	PacketCallback* pm = PacketCallback::getPacketCallback ();
 	// Configuration des callback
 	pm->addOperation (areYouReady, callbackAreYouReady);
 	pm->addOperation (sendOperation, callbackSendOperation);
 	
-	// création d'un paquet
-	//Packet p = PacketAreYouReady (5);
-	Chunk chunk ("Hello", 5);
-	Packet p = PacketSendOperation("192.168.0.2", chunk);
+// création d'un paquet
+	PacketAreYouReady p (5);	
+	//PacketSendOperation p ("192.168.0.2", Chunk());
+	
 	
 	// préparation envoie
 	Data d = p.serialize ();
 	
 	cout << "taille du packet : " << d.getSize () << endl;
-	cout << "Contenu du packet : " << d.getString () << endl;
+	//cout << "Contenu du packet : " << d.getString () << endl;
 	
-	//reception
+	cout << "ENVOI" << endl;
+	
+//reception
 	Packet pp(d);
 	
-	// Analyse du paquet
+// Analyse du paquet
 	pm->packetOperation (pp);
 	
-	// Destruction du manager
+// Destruction du manager
 	PacketCallback::quit ();
-	
-	
 	
 	
 	
