ef1af3fc823826c6544f801464ce16468d313422
olivier <olivier.oblin@gmail.com>
2012-05-14 23:44:42 +0200

    upgrade du logger avec des flux + etat verbose + commande raccourcis type endl(endLog)

--- logger/src/example.cpp
+++ logger/src/example.cpp
@@ -17,5 +17,20 @@ int main(int argc, char* argv[])
 	logger.put("notice", "a small insert was detected...");
 	logger.put("error", "hacking attempt.");
 	
+	
+	// nouvelle méthode
+	
+	
+	logger<<"sdfsdf"<<155<<12.5<<" "<<"qsdf e"<<endLog;
+	logger<<error<<"Probleme de chargement"<<endLog;
+	logger<<critical<<"crash du system principal"<<endLog;
+	logger<<alert<<"Surcharge du serveur"<<endLog;
+	
+	logger.setVerboseMode(true);
+	logger<<normal<<"message normal affiché grace à l'activation du verbose"<<endLog;
+	logger<<"sdfsdf"<<155<<12.5<<" "<<"qsdf e"<<endLog;
+	
+	
+	
 	return 0;
 }
