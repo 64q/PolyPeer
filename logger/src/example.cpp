d172e3a74e0e5d7734e75b69d08dbd6174024bfd
KenTiN <quentin.lebourgeois@gmail.com>
2012-05-22 18:51:54 +0200

    remise a niveau logger

--- logger/src/example.cpp
+++ logger/src/example.cpp
@@ -14,23 +14,14 @@ int main(int argc, char* argv[])
 	
 	Logger logger("log/example.log");
 	
-	logger.put("notice", "a small insert was detected...");
-	logger.put("error", "hacking attempt.");
-	
-	
-	// nouvelle méthode
-	
-	
-	logger<<"sdfsdf"<<155<<12.5<<" "<<"qsdf e"<<endLog;
-	logger<<error<<"Probleme de chargement"<<endLog;
-	logger<<critical<<"crash du system principal"<<endLog;
-	logger<<alert<<"Surcharge du serveur"<<endLog;
+	logger << error << "Probleme de chargement" << endlog;
+	logger << critical << "Crash du system principal" <<endlog;
+	logger << alert << "Surcharge du serveur" << endlog;
 	
 	logger.setVerboseMode(true);
-	logger<<normal<<"message normal affiché grace à l'activation du verbose"<<endLog;
-	logger<<"sdfsdf"<<155<<12.5<<" "<<"qsdf e"<<endLog;
-	
-	
+	logger << normal << "Message normal affiché grace à l'activation du verbose" << endlog;
+	logger << debug << "Message de debuggage" << endlog;
+	logger << notice << "Information de changement d'état" << endlog;
 	
 	return 0;
 }
