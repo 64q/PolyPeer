027092d5a6e9ef784fc7b77686afe27af8647237
Lulububu <erwan.matrat@cegetel.net>
2012-05-26 17:54:03 +0200

    changement droit

--- filemanager/src/FileManager.cpp
+++ filemanager/src/FileManager.cpp
@@ -297,7 +297,7 @@ void FileManager::createDirectory(std::string pathDirectory, std::string current
     #ifdef WIN32
 		CreateDirectory(tmp.c_str(), NULL);
     #elif defined(linux)
-        mkdir(tmp.c_str(),755);
+        mkdir(tmp.c_str(),S_IRWXU|S_IRWXG|S_IRWXO);
     #endif
 		createDirectory(pathDirectory.substr(ind+1), tmp+"/");
 	}
