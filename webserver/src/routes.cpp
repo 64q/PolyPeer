72b49b56f63ee6b3a0f922d31ca9c3034a56f76b
KenTiN <quentin.lebourgeois@gmail.com>
2012-05-27 16:45:31 +0200

    affichage topologie + déploiements + host (wip)

--- webserver/src/routes.cpp
+++ webserver/src/routes.cpp
@@ -5,9 +5,8 @@
 
 #include <mongoose.h>
 
-/*TODO
 #include <PolypeerServer.hpp>
-#include <ServerData.hpp>*/
+#include <ServerData.hpp>
 #include <routes.hpp>
 
 using namespace std;
@@ -29,21 +28,27 @@ void deployments_route(mg_connection* conn, const mg_request_info* request_info)
 	mg_printf(conn, "%s", ajax_reply_start);
 	mg_printf(conn, "[");
 
-	/*
 	PolypeerServer* server = PolypeerServer::getInstance();
 	
 	ServerData& data = server->getServerData();
 	
 	vector<File*>* files = data.getDeployFiles();
-	for (vector<File*>::iterator it = files->begin(); it != files->end(); it++) {
+	for (vector<File*>::iterator it = files->begin(); it != files->end(); it++) 
+	{
 		FileManager* fm = (*it)->getFileManager();
-		mg_printf(conn, "{\"id\":%i, \"filename\":%s}", fm->getIdFile(), fm->getFileName().c_str());
+		mg_printf(conn, "{\"id\":%i, \"name\":\"%s\", \"state\":\"%s\", \"filename\":\"%s\"}"
+			, fm->getIdFile()
+			, (*it)->getName().c_str()
+			, getStringFileState((*it)->getFileState()).c_str()
+			, fm->getFileName().c_str()
+		);
 		
-		if (it != files->end()--) {
+		if ((it + 1) != files->end()) 
+		{
 			mg_printf(conn, ",");
 		}
 	
-	}*/
+	}
 	
 	mg_printf(conn, "]");
 }
@@ -62,22 +67,97 @@ void get_stats_route(mg_connection* conn, const mg_request_info* request_info)
 
 void get_host_route(mg_connection* conn, const mg_request_info* request_info)
 {
+	char qip[32];
+	
+	get_qsvar(request_info, "ip", qip, sizeof(qip));
+	
+	PolypeerServer* server = PolypeerServer::getInstance();
+	ServerData& data = server->getServerData();
+	Entity* entity = data.getHostByAddress(qip);
+	std::vector<DeploymentState>* deploys = entity->getDeploys();
+
 	mg_printf(conn, "%s", ajax_reply_start);
-	mg_printf(conn, "{\"ip\":\"192.168.0.10\", \"name\":\"irc007-02\", \"state\": \"online\", \"deployments\": [{\"name\":\"test\"}]}");
+	mg_printf(conn, "{\"ip\":\"%s\", \"name\":\"%s\", \"state\": \"%s\", \"deployments\": ["
+		, entity->getIP()->c_str(), entity->getName().c_str()
+		, getStringHostState(entity->getHostState()).c_str()
+	);
+	
+	for (std::vector<DeploymentState>::iterator it = deploys->begin(); it != deploys->end(); it++) {
+		File* f = it->getRefFile();
+		mg_printf(conn, "{\"name\":\"%s\", \"state\":\"%s\", \"current\":\"%i\", \"total\":\"%lu\"}"
+			, f->getName().c_str()
+			, getStringHostDeployState(it->getCurrentState()).c_str()
+			, it->getCurrentIdChunk()
+			, f->getFileManager()->getNumberChunk()
+		);
+		
+		if ((it + 1) != deploys->end()) 
+		{
+			mg_printf(conn, ",");
+		}
+	}
+	
+	mg_printf(conn, "]}");
 }
 
 void deployment_route(mg_connection* conn, const mg_request_info* request_info)
 {
+	char qid[16];
+	
+	get_qsvar(request_info, "id", qid, sizeof(qid));
+	
 	mg_printf(conn, "%s", ajax_reply_start);
-	mg_printf(conn, "{\"name\":\"trololo\"}");
+	mg_printf(conn, "{\"id\":\"%s\", \"name\":\"trololo\"}", qid);
+}
+
+void displayEntity(mg_connection* conn, Entity* entity)
+{
+	if (entity->getType() == ZONE)
+	{
+		map<string, Entity*>* entities = entity->getEntities();
+		
+		mg_printf(conn, "{\"name\":\"%s\", \"type\":\"zone\", \"nc\":%i, \"cbbs\":%f, \"hosts\":[", entity->getName().c_str(), entity->getNetworkCapacity(), entity->getCurrentBroadbandSpeed());
+		
+		for (map<string, Entity*>::iterator it = entities->begin(); it != entities->end();) 
+		{
+			displayEntity(conn, (*it).second);
+			
+			if ((++it) != entities->end()) 
+			{
+				mg_printf(conn, ",");
+			}
+		}
+		
+		mg_printf(conn, "]}");
+	}
+	else
+	{
+		mg_printf(conn, "{\"name\":\"%s\", \"type\":\"host\", \"ip\":\"%s\", \"state\":\"%s\"}"
+			, entity->getName().c_str(), entity->getIP()->c_str()
+			, getStringHostState(entity->getHostState()).c_str()
+		);
+	}
 }
 
 void network_route(mg_connection* conn, const mg_request_info* request_info)
 {
+	PolypeerServer* server = PolypeerServer::getInstance();
+	ServerData& data = server->getServerData();
+	map<string, Entity*>* entities = data.getEntities();
+	
 	mg_printf(conn, "%s", ajax_reply_start);
 	mg_printf(conn, "[");
-	mg_printf(conn, "{\"id\":\"1\", \"type\":\"zone\", \"name\":\"Zone1\", \"hosts\":[{\"ip\":\"192.168.0.2\", \"name\":\"irc001-01\", \"type\":\"host\", \"state\": \"online\"}]},");
-	mg_printf(conn, "{\"id\":\"2\", \"name\":\"TPRESAU1\", \"type\":\"zone\", \"hosts\":[{\"ip\":\"192.168.0.12\", \"name\":\"irc002-01\", \"type\":\"host\", \"state\": \"online\"}, {\"ip\":\"192.168.0.10\", \"name\":\"irc002-03\", \"type\":\"host\",\"state\": \"offline\"}]}");
+	
+	for (map<string, Entity*>::const_iterator it = entities->begin(); it != entities->end();) 
+		{
+			displayEntity(conn, (*it).second);
+			
+			if ((++it) != entities->end()) 
+			{
+				mg_printf(conn, ",");
+			}
+		}
+	
 	mg_printf(conn, "]");
 }
 
@@ -108,5 +188,38 @@ void new_deployment_route(mg_connection* conn, const mg_request_info* request_in
 	
 	mg_printf(conn, "%s", ajax_reply_start);
 	mg_printf(conn, "{\"state\":\"done\"}");
+}
+
+void stop_route(mg_connection* conn, const mg_request_info* request_info)
+{
+	PolypeerServer* server = PolypeerServer::getInstance();
+	
+	server->stop();
+}
+
+void pause_route(mg_connection* conn, const mg_request_info* request_info)
+{
+	// TODO
+}
+
+void restart_route(mg_connection* conn, const mg_request_info* request_info)
+{
+	PolypeerServer* server = PolypeerServer::getInstance();
+	
+	server->restart();
+}
+
+void delete_deployment_route(mg_connection* conn, const mg_request_info* request_info)
+{
+	PolypeerServer* server = PolypeerServer::getInstance();
+	
+	ServerData& data = server->getServerData();
+	
+	
+}
+
+void pause_deployment_route(mg_connection* conn, const mg_request_info* request_info)
+{
+	// TODO
 }
 
