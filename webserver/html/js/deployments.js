0fbcb71fd884c0da0be5fca55b6b4ff9b862dbc3
KenTiN <quentin.lebourgeois@gmail.com>
2012-05-29 20:10:56 +0200

    correction code bouton affichage retour

--- webserver/html/js/deployments.js
+++ webserver/html/js/deployments.js
@@ -40,6 +40,7 @@
 $('#pause-button').addEventListener('click', function() {
 	if (confirm('Etes-vous sûr de vouloir mettre en pause le serveur ?')) {
 		Ajax.request('/ajax/pause_deployments', null, function(content) {
+			var content = JSON.parse(content);
 			if (content.state == "done") {
 				notifySuccess("Les déploiements ont été mis en pause");
 			} else {
