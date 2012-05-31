7bc98af0d1340eace0059760d450c43142705e58
olivier <olivier.oblin@gmail.com>
2012-06-01 00:41:09 +0200

    correction packet et optimisation algo déploiment

--- packet/src/Packet.cpp
+++ packet/src/Packet.cpp
@@ -5,25 +5,33 @@
 #include <iostream>
 #include <cstring>
 #include <string>
+#include <math.h>
 
 // Project header
 
 using namespace std;
 
-Packet::Packet() : myType(EUndefined), myReadingPosition(0), valid(true), address("255.255.255.255")
+Packet::Packet() : myType(EUndefined), myReadingPosition(0), valid(true), address("")
 {
 
 }
 
-Packet::Packet(const Data& d) : myType(EUndefined), myReadingPosition(0), valid(true), address("255.255.255.255")
+Packet::Packet(const Data& d) : myType(EUndefined), myReadingPosition(0), valid(true), address("")
 {
 	unserialize (d);
 }
 
-Packet::Packet(const char* s, unsigned int size) : myType(EUndefined), myReadingPosition(0), valid(true), address("255.255.255.255")
+Packet::Packet(const char* s, unsigned int size) : myType(EUndefined), myReadingPosition(0), valid(true), address("")
 {
-	Data d (s, size);
-	unserialize (d);
+	// alloc de 5M max : après ca devient plus soutenable
+	if((size > 0) && (size < 5000000))
+	{
+		Data d (s, size);
+		unserialize (d);
+	} else
+	{
+		valid = false;
+	}
 }
 
 Packet::~Packet()
@@ -33,28 +41,76 @@ Packet::~Packet()
 Data Packet::serialize ()
 {
 	Data data;
+	// espace pour insérer la taille du paquet -> 32 bits
+	// on le fait en caractere pour simplifier (trop de pb avec le décalage de bit)
+	for(int j = 0; j < 32;j++)
+		data << '0';
+	// taille du tableau pour vérif
 	data << typeToString(myListData.size());
 	data << '/';
+	// type de paquet
 	data << typeToString(int(myType));
 	data << '/';
+	// insertion des données
 	for (unsigned int i = 0; i < myListData.size(); i++)
 	{
 		data << typeToString(myListData[i].getSize());
 		data << '/';
 		data << myListData[i];
 	}
+	
+	// récupération de la taille du paquet
+	int size = data.getSize();
+	
+	// écriture de la taille sur les 32 premiers octets
+	for (unsigned int cpt = 0; cpt < 32; cpt++)
+	{
+		if(size == 0)
+			break;
+		if( size%2 == 1)
+		{
+			data[31-cpt] = '1';
+			size--;
+		}
+		else
+			data[31-cpt] = '0';
+		size=size/2;
+	}
+	
 	return data;
 }
 
 int Packet::unserialize (const Data& d)
 {
-	// init
+	// faire un pré-test -> un paquet sérialisé a une taille mini sûr de 32 octets
+	if (d.getSize() <= 32)
+	{
+		valid = false;
+		return 0;
+	}
+
+	// vérifier la taille du paquet (les 32 premiers octets)
+	unsigned int packetSize = 0;
+	for (unsigned int cpt = 0; cpt < 32; cpt++)
+	{
+		if(d[31-cpt] == '1')
+			packetSize += pow(2,cpt);
+	}
+	
+	if(d.getSize() != packetSize)
+	{
+		valid = false;
+		return 0;
+	}
+
+
+	// EXTRACTION DES DONNEES
 	myReadingPosition = 0;
 	myListData.clear ();
 	
 	// var
 	string extractString; // extraction de valeur
-	unsigned int posInData = 0;
+	unsigned int posInData = 32;
 	int nbExtraction = 0;
 	int nbData;
 	int size;
