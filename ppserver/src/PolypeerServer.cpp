5ce57faf4f5ff7fee4ad5e422104fed41b031bf4
Lulububu <erwan.matrat@cegetel.net>
2012-06-01 23:25:23 +0200

    utilisation WOL dans initialisation connection (pas teste)

--- ppserver/src/PolypeerServer.cpp
+++ ppserver/src/PolypeerServer.cpp
@@ -27,10 +27,10 @@ PolypeerServer::PolypeerServer() :
 {
 	logger.setVerboseMode(true);
 	logger << "Le serveur Polypeer a été démarré sur le port " << this->port << endlog;
-	
+
 	// récupération du logger
 	webserver = WebServer::getInstance();
-	
+
 	// initialisation du system de callBack
 	// -> permet l'appel "automatique des traitement pour un paquet
 	PacketCallback * pcb = PacketCallback::getPacketCallback();
@@ -69,10 +69,10 @@ void PolypeerServer::start()
 	this->running = true;
 	// Lancement du webserveur
 	webserver->start();
-	
+
 	// initialisation des connexions
 	initConnections();
-	
+
 	// Lancement du server
 	this->run();
 }
@@ -89,11 +89,11 @@ void PolypeerServer::run()
 	// propre à 'instance serveur polypeer
 	PolypeerServer* server = PolypeerServer::getInstance();
 	ServerData& data = server->getServerData();
-	
+
 	// Algorithme de déploiment
 	DeploymentAlgorithm* algo = new ShareDeployment(server, &data);
-	
-	
+
+
 	PolypeerServer::getInstance()->logger<< "waiting 2 seconds to bind socket..." << endlog;
 	PolypeerServer::getInstance()->multiSleep(2000);
 
@@ -102,11 +102,11 @@ void PolypeerServer::run()
 
 		// Execution d'une étape de l'algo
 		algo->nextStep();
-		
+
 		// waiting time
 		//PolypeerServer::getInstance()->multiSleep(100);
 	}
-	
+
 	delete algo;
 }
 
@@ -122,25 +122,25 @@ void  PolypeerServer::initConnections()
 {
 	// liste des déploiements
 	vector<File*>* files = sData->getDeployFiles();
-	
+
 	ConnectionManager* cm = sData->getConnectionManager();
-	
+
 	set<Entity*> myHostSet;
-	
-	for (vector<File*>::iterator itFile = files->begin(); itFile != files->end(); itFile++) 
+
+	for (vector<File*>::iterator itFile = files->begin(); itFile != files->end(); itFile++)
 	{
 		if(((*itFile)->getFileState() == READY) || ((*itFile)->getFileState() == DEPLOYMENT))
 		{
 			vector<Entity*>* hosts = (*itFile)->getDeploysOn();
-		
-			for (vector<Entity*>::iterator itHost = hosts->begin(); itHost != hosts->end(); itHost++) 
+
+			for (vector<Entity*>::iterator itHost = hosts->begin(); itHost != hosts->end(); itHost++)
 			{
 				myHostSet.insert((*itHost));
 			}
 		}
 	}
-	
-	for (set<Entity*>::iterator itHost = myHostSet.begin(); itHost != myHostSet.end(); itHost++) 
+
+	for (set<Entity*>::iterator itHost = myHostSet.begin(); itHost != myHostSet.end(); itHost++)
 	{
 		pthread_t myThread;
 		if(cm->getConnection((*itHost)->getIP()) == NULL)
@@ -156,16 +156,22 @@ void* thread_initConnection(void* data)
 	ConnectionManager* cm = (PolypeerServer::instance)->getServerData().getConnectionManager();
 	Host* myHost = (Host*)data;
 	try
-	{	
+	{
 		Socket* socket = new Socket(myHost->getIP(), (PolypeerServer::instance)->getServerData().getClientPort());
 		cm->addConnection(myHost->getIP(), socket);
 		myHost->setHostState(WAIT);
 	} catch(ConnectionException)
 	{
 		//(PolypeerServer::instance)->getLogger()<<"Connection to " << myHost->getIP() << " failed" <<endlog;
+
+
+		/*A tester en condition réelle!!
+
+		ConnectionManager::WOL(myHost->getMAC(), myHost->getMask());
+		*/
 	}
 
-	
+
 	return NULL;
 }
 
