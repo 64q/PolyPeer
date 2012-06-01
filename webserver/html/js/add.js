8cce7e746c012a4f0fcfd40f74efd2b045871e27
KenTiN <quentin.lebourgeois@gmail.com>
2012-06-01 17:55:43 +0200

    correction add deployment

--- webserver/html/js/add.js
+++ webserver/html/js/add.js
@@ -3,79 +3,80 @@
  * Quentin Lebourgeois - 2012
  */
  
-function createInputZone(parent, item) {
+(function() {
+	function createInputZone(parent, item) {
 	
-	var zone = document.createElement('p');
-	var title = document.createElement('label');
-	var input = document.createElement('input');
+		var zone = document.createElement('p');
+		var title = document.createElement('label');
+		var input = document.createElement('input');
 	
-	input.type = "checkbox"; input.value = item.name;
+		input.type = "checkbox"; input.value = item.name;
 	
-	title.className = "checkbox";
-	title.appendChild(input);
-	title.appendChild(document.createTextNode(item.name));
+		title.className = "checkbox";
+		title.appendChild(input);
+		title.appendChild(document.createTextNode(item.name));
 	
-	zone.appendChild(title);
+		zone.appendChild(title);
 	
-	for (var i = 0; i < item.hosts.length; i++) {
-		if (item.hosts[i].type == "zone") {
-			createInputZone(zone, item.hosts[i]);
+		for (var i = 0; i < item.hosts.length; i++) {
+			if (item.hosts[i].type == "zone") {
+				createInputZone(zone, item.hosts[i]);
+			}
 		}
+
+		parent.appendChild(zone);
 	}
 
-	parent.appendChild(zone);
-}
-
-$('#new-deployment-form').addEventListener('submit', function(e) {
- 	var name = $('#name');
- 	var path = $('#path');
- 	var cpath = $('#cpath');
- 	var error = false;
- 	
- 	var checkboxes = qsa('#new-deployment-form input[type="checkbox"]');
-	var zones = [];
+	$('#new-deployment-form').addEventListener('submit', function(e) {
+	 	var name = $('#name');
+	 	var path = $('#path');
+	 	var cpath = $('#cpath');
+	 	var error = false;
+	 	
+	 	var checkboxes = qsa('#new-deployment-form input[type="checkbox"]');
+		var zones = [];
 	
-	for (var i = 0; i < checkboxes.length; i++) {
-		if (checkboxes[i].checked) {
-			zones.push(checkboxes[i].value);
+		for (var i = 0; i < checkboxes.length; i++) {
+			if (checkboxes[i].checked) {
+				zones.push(checkboxes[i].value);
+			}
 		}
-	}
 	
-	var str_zones = zones.join(',');
- 	
- 	if (zones.length == 0) {
- 		notifyError('Veuillez cocher au moins une zone');
- 		error = true;
- 	} else if (name.value == "") {
- 		notifyError('Veuillez donner un nom au déploiement');
- 		error = true;
- 	} else if (path.value == "") {
- 		notifyError('Veuillez donner un chemin vers le fichier');
- 		error = true;
- 	} else if (cpath.value == "") {
- 		notifyError('Veuillez donner un chemin où mettre le fichier chez le client');
- 		error = true;
- 	}
- 	
- 	if (!error) {
-	 	PolyPeerJS.Ajax('/ajax/new_deployment', 'name=' + name.value + '&spath=' + path.value + '&zones=' + str_zones + '&cpath=' + cpath.value, function(content) {
-	 		if (content.state == "done") {
-	 			notifySuccess('Création du déploiement exectuée.');
-	 		}
-	 	}, function() {
-	 			notifyError('Impossible de créer le nouveau déploiement');
-	 	});
-	}
- 	
- 	e.preventDefault();
- });
- 
-PolyPeerJS.Ajax('/ajax/network', null, function(content) {
-	var view = $('#zones-choice');
-	for (var i = 0; i < content.length; i++) {
-		if (content[i].type == "zone") {
-			createInputZone(view, content[i]);
+		var str_zones = zones.join(',');
+	 	
+	 	if (zones.length == 0) {
+	 		notifyError('Veuillez cocher au moins une zone');
+	 		error = true;
+	 	} else if (name.value == "") {
+	 		notifyError('Veuillez donner un nom au déploiement');
+	 		error = true;
+	 	} else if (path.value == "") {
+	 		notifyError('Veuillez donner un chemin vers le fichier');
+	 		error = true;
+	 	} else if (cpath.value == "") {
+	 		notifyError('Veuillez donner un chemin où mettre le fichier chez le client');
+	 		error = true;
+	 	}
+	 	
+	 	if (!error) {
+		 	PolyPeerJS.Ajax('/ajax/new_deployment', 'name=' + name.value + '&spath=' + path.value + '&zones=' + str_zones + '&cpath=' + cpath.value, function(content) {
+		 		if (content.state == "done") {
+		 			notifySuccess('Création du déploiement exectuée.');
+		 		}
+		 	}, function() {
+		 			notifyError('Impossible de créer le nouveau déploiement');
+		 	});
 		}
-	}
-});
-
+	 	
+	 	e.preventDefault();
+	 });
+	 
+	PolyPeerJS.Ajax('/ajax/network', null, function(content) {
+		var view = $('#zones-choice');
+		for (var i = 0; i < content.length; i++) {
+			if (content[i].type == "zone") {
+				createInputZone(view, content[i]);
+			}
+		}
+	});
+})();
