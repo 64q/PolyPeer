d6088a3b7e6ae217bb218a4b8194fbb3970d717d
olivier <olivier.oblin@gmail.com>
2012-06-03 02:46:05 +0200

    ajout des scripts de service client et serveur et leger ajustement sur le time des sockets

---
+++ ppclient/pclientservice.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Gestion du client PolyPeer en tant que service unix
+# bash par Olivier
+
+
+# Vérifier que l'utilisateur est bien root
+#if (( `/usr/bin/id -u` != 0 )); then { echo "Il faut passer en root !!!"; exit; } fi
+
+# Parametre du serveur
+
+#Port d'écoute du client PolyPeer
+PCLIENTLISTEN=5555
+#Port d'écoute du server PolyPeer
+PSERVERDEST=6666
+#Port d'écoute du server PolyPeer
+ADDRSERVER="192.168.0.14"
+#Repertoire racine pour le démon
+CHROOTDOS=`pwd`
+#Repertoire qui contient le programme executable 'ppclient'
+PWDPCLIENT=`pwd`
+
+
+start() {
+PID=`cat ppclient.pid`
+res=`ps -p $PID | grep ppclient`
+
+if [ "$res" != "" ]; then
+	echo "PolyPeer client is already running"
+else
+	$PWDPCLIENT/ppclient -d -r $CHROOTDOS -c $PCLIENTLISTEN -p $PSERVERDEST  -s $ADDRSERVER
+	echo "PolyPeer client start..."
+fi
+
+}
+
+stop() {
+PID=`cat ppclient.pid`
+res=`ps -p $PID | grep ppclient`
+
+if [ "$res" != "" ]; then
+	kill -2 $PID
+	kill -9 $PID
+	echo "PolyPeer client stop..."
+else
+	echo "nothing to stop"
+fi
+}
+
+status() {
+PID=`cat ppclient.pid`
+res=`ps -p $PID | grep ppclient`
+
+if [ "$res" != "" ]; then
+	echo "PolyPeer client running on PID $PID"
+else
+	echo "PolyPeer client is not currently running"
+fi
+}
+
+
+case "$1" in
+	'start')
+		start
+		;;
+	'stop')
+		stop
+		;;
+	'restart')
+		stop 
+		echo "wait 5 seconds"
+		sleep 5
+		start
+		;;
+	'status')
+		status
+		;;
+	*)
+		echo "Usage $0 {start|stop|restart|status}"
+esac
+ 
+exit 0
---
+++ ppserver/pserverservice.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+#
+# Gestion du serveur PolyPeer en tant que service
+# bash par Olivier
+
+
+# Vérifier que l'utilisateur est bien root
+#if (( `/usr/bin/id -u` != 0 )); then { echo "Il faut passer en root !!!"; exit; } fi
+
+# Parametre du serveur
+
+#Port d'écoute du serveur PolyPeer
+PSERVERLISTEN=6666
+#Port d'écoute des clients PolyPeer
+PCLIENTDEST=5555
+#Port du server WEB -> ne peut pas etre changé actuellement
+PWEBSERVER=6969
+#Repertoire racine pour le démon -> bien vérifier que les 
+#config soit dans le repertoire sinon le serveur ne se lance pas
+CHROOTDOS=`pwd`
+#Repertoire qui contient le programme executable 'ppserver'
+PWDPPSERVER=`pwd`
+
+
+start() {
+PID=`cat ppserver.pid`
+res=`ps -p $PID | grep ppserver`
+
+if [ "$res" != "" ]; then
+	echo "PolyPeer server is already running"
+else
+	`$PWDPPSERVER/ppserver -d -r $CHROOTDOS -s $PSERVERLISTEN -c $PCLIENTDEST -w $PWEBSERVER`
+	echo "PolyPeer server start..."
+	
+fi
+}
+
+stop() {
+PID=`cat ppserver.pid`
+res=`ps -p $PID | grep ppserver`
+
+if [ "$res" != "" ]; then
+	kill -2 $PID
+	echo "PolyPeer server stop..."
+else
+	echo "nothing to stop"
+fi
+}
+
+status() {
+PID=`cat ppserver.pid`
+res=`ps -p $PID | grep ppserver`
+
+if [ "$res" != "" ]; then
+	echo "PolyPeer server running on PID $PID"
+else
+	echo "PolyPeer server is not currently running"
+fi
+}
+
+
+case "$1" in
+	'start')
+		start
+		;;
+	'stop')
+		stop
+		;;
+	'restart')
+		stop 
+		echo "wait 5 seconds"
+		sleep 5
+		start
+		;;
+	'status')
+		status
+		;;
+	*)
+		echo "Usage $0 {start|stop|restart|status}"
+esac
+ 
+exit 0
